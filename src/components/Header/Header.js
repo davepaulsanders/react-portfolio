a26513ef5bd4ecdfe4d0dc17565a413190e74e1d
Dave Sanders <davepaulsanders@gmail.com>
2022-06-21 11:41:16 -0400

    Nav bar toggles and is responsive

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,31 +1,47 @@
-import React from "react";
+import React, { useState } from "react";
 import "bootstrap/dist/css/bootstrap.css";
 
 import "./Header.css";
 
 function Header() {
+  const [menuStatus, setMenuStatus] = useState(false);
+  const links = ["About", "Projects", "Contact", "Resume"];
+  const toggleMenu = (e) => {
+    e.preventDefault();
+    if (menuStatus) {
+      document.querySelector(".nav-links").style.display = "none";
+    } else {
+      document.querySelector(".nav-links").style.display = "block";
+    }
+    setMenuStatus(!menuStatus);
+  };
+
   return (
-    <div className="w-100 d-flex justify-content-between align-items-center">
-      <h2 className="site-title">Dave Sanders</h2>
-      <nav className="d-flex align-items-center">
-        <ul className="nav-links d-flex list-unstyled">
-          <li className="p-1 me-5">
-            <a className="text-decoration-none text-black" href="#about">
-              About
-            </a>
-          </li>
-          <li className="p-1 ms-5 me-5">
-            <a className="text-decoration-none text-black" href="#projects">
-              Projects
-            </a>
-          </li>
-          <li className="p-1 ms-5 me-5">
-            <a className="text-decoration-none text-black" href="#contact">
-              Contact
-            </a>
-          </li>
-        </ul>
-          <i class="hamburger fa-solid fa-bars fa-2xl d-none"></i>
+    <div>
+      <nav className="w-100 d-flex justify-content-between align-items-center">
+        <h1 className="site-title no-wrap">
+          <a className="text-decoration-none text-black" href="/">Dave Sanders</a>
+        </h1>
+        <i
+          onClick={toggleMenu}
+          className="hamburger fa-solid fa-bars fa-2xl d-none"
+        ></i>
+        <div className="nav-links">
+          <ul className="d-flex list-unstyled">
+            {links.map((link) => {
+              return (
+                <li key={link} className="">
+                  <a
+                    className="links text-decoration-none text-black"
+                    href={link}
+                  >
+                    {link}
+                  </a>
+                </li>
+              );
+            })}
+          </ul>
+        </div>
       </nav>
     </div>
   );
