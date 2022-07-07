b4dcd99e6cae601ccb112ab0b794d12783d24792
Dave Sanders <davepaulsanders@gmail.com>
2022-07-07 11:24:28 -0400

    Removing resume embed due to line break issues

--- src/components/Resume/Resume.js
+++ src/components/Resume/Resume.js
@@ -3,14 +3,8 @@ import "./Resume.css";
 const resume = require("../../assets/resume.png");
 const Resume = () => {
   return (
-    <div className="d-flex flex-column justify-content-center">
+    <div className="d-flex flex-column justify-content-center align-items-center">
       <h2 className="resume-title text-center">Resume</h2>
-      <iframe
-        title="resume"
-        width="780"
-        height="1000"
-        src="https://docs.google.com/document/d/e/2PACX-1vR--kEgCaoY8xdJdcFx3CBLa-MwZXXzQgLl3uQHPuFzJ_3g9IlGwR5VC6t2jkVPVw/pub?embedded=true"
-      ></iframe>
       <button
         className="resume-download btn btn-primary"
         onClick={() =>
@@ -18,7 +12,7 @@ const Resume = () => {
             "https://drive.google.com/file/d/11O3VWVLkBoGccaZNRg5fWTp6dISA-pvD/view?usp=sharing")
         }
       >
-        Resume <img className="resume-icon" src={resume} alt="resume" />
+        Download <img className="resume-icon" src={resume} alt="resume" />
       </button>
     </div>
   );
