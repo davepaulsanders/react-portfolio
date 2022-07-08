de4c3d92c960d180585613579ff87c66605b3fbc
Dave Sanders <davepaulsanders@gmail.com>
2022-07-08 12:17:19 -0400

    Updates

---
+++ precache-manifest.f73f79217ec4c5875efbbef7edcc8e0d.js
@@ -0,0 +1,62 @@
+self.__precacheManifest = [
+  {
+    "revision": "68f6ee81b1c8df144fdc",
+    "url": "/react-portfolio/static/css/main.05e6389e.chunk.css"
+  },
+  {
+    "revision": "68f6ee81b1c8df144fdc",
+    "url": "/react-portfolio/static/js/main.68f6ee81.chunk.js"
+  },
+  {
+    "revision": "98fae294dabfb7543782",
+    "url": "/react-portfolio/static/js/1.98fae294.chunk.js"
+  },
+  {
+    "revision": "e721351d54e67753c356",
+    "url": "/react-portfolio/static/css/2.94d66bfc.chunk.css"
+  },
+  {
+    "revision": "e721351d54e67753c356",
+    "url": "/react-portfolio/static/js/2.e721351d.chunk.js"
+  },
+  {
+    "revision": "fb8d19f71ffd14d5e37a",
+    "url": "/react-portfolio/static/js/runtime~main.fb8d19f7.js"
+  },
+  {
+    "revision": "88671dc1cd401060dcdcece9ead0a8a0",
+    "url": "/react-portfolio/static/media/small-headshot.88671dc1.jpeg"
+  },
+  {
+    "revision": "bbedc41f556bbf801c3c11f71f80d603",
+    "url": "/react-portfolio/static/media/github.bbedc41f.png"
+  },
+  {
+    "revision": "b9ca1060b4c73b08ca8a41227d4a0939",
+    "url": "/react-portfolio/static/media/tech-nically.b9ca1060.png"
+  },
+  {
+    "revision": "a96be8a62dbce2c71860d1de2b2c0dc3",
+    "url": "/react-portfolio/static/media/grade-gatsby.a96be8a6.png"
+  },
+  {
+    "revision": "88ead49a6e604f8e8ff07cf22d7543c4",
+    "url": "/react-portfolio/static/media/nba.88ead49a.png"
+  },
+  {
+    "revision": "349548b2600c2726847753707d487a8b",
+    "url": "/react-portfolio/static/media/employee-tracker.349548b2.png"
+  },
+  {
+    "revision": "a4fbe15dc54359727c20c48b16f71b7a",
+    "url": "/react-portfolio/static/media/nba-standings.a4fbe15d.png"
+  },
+  {
+    "revision": "7d7e6ec150e8bb3436f72f0bbab9f4e6",
+    "url": "/react-portfolio/static/media/weather-dashboard.7d7e6ec1.png"
+  },
+  {
+    "revision": "ca0fa6be43ade936726ca857354297f0",
+    "url": "/react-portfolio/index.html"
+  }
+];
