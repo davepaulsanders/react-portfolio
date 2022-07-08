bdf15e79894deed93040b4ac7fa96575ceafbf57
Dave Sanders <davepaulsanders@gmail.com>
2022-07-08 13:15:23 -0400

    Changing display

--- src/components/Resume/Resume.js
+++ src/components/Resume/Resume.js
@@ -6,7 +6,7 @@ const Resume = () => {
   return (
     <div className="d-flex flex-column justify-content-center align-items-center">
       <h2 className="resume-title text-center">Resume</h2>
-      <button className="resume-link btn btn-primary">Download</button>
+      <button className="resume-link d-block btn btn-primary">Download</button>
     </div>
   );
 };
