a9af71077d6f93071731373b76227edc8bf3ff52
Dave Sanders <davepaulsanders@gmail.com>
2022-07-08 13:17:26 -0400

    Fixed button display issue

--- src/components/Resume/Resume.js
+++ src/components/Resume/Resume.js
@@ -1,12 +1,19 @@
 import React from "react";
 import "./Resume.css";
-
 const resume = require("../../assets/resume.png");
 const Resume = () => {
   return (
     <div className="d-flex flex-column justify-content-center align-items-center">
       <h2 className="resume-title text-center">Resume</h2>
-      <button className="resume-link d-block btn btn-primary">Download</button>
+      <button
+        className="resume-link btn btn-primary"
+        onClick={() =>
+          (window.location.href =
+            "https://drive.google.com/file/d/11O3VWVLkBoGccaZNRg5fWTp6dISA-pvD/view?usp=sharing")
+        }
+      >
+        Download <img className="resume-icon" src={resume} alt="resume" />
+      </button>
     </div>
   );
 };
