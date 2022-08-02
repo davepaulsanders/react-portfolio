d9ef6efb012a2e7893abf1e163e2b9be1d1cd65d
Dave Sanders <davepaulsanders@gmail.com>
2022-08-02 16:14:37 -0400

    Changing about Link href

--- src/App.js
+++ src/App.js
@@ -11,7 +11,7 @@ import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 
 function App() {
   const [links] = useState([
-    { page: "About", link: "/" },
+    { page: "About", link: "/about" },
     { page: "Projects", link: "/projects" },
     { page: "Skills", link: "/skills" },
     { page: "Resume", link: "/resume" },
