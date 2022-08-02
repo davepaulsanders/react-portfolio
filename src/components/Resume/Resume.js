067f0f4b9b56ca990ff389caf0faddca91abc440
Dave Sanders <davepaulsanders@gmail.com>
2022-08-02 17:34:56 -0400

    Changing links to open new tabs including resume

--- src/components/Resume/Resume.js
+++ src/components/Resume/Resume.js
@@ -5,15 +5,14 @@ const Resume = () => {
   return (
     <div className="d-flex flex-column justify-content-center align-items-center">
       <h2 className="resume-title text-center">Resume</h2>
-      <button
-        className="resume-link d-block btn btn-primary"
-        onClick={() =>
-          (window.location.href =
-            "https://drive.google.com/file/d/11O3VWVLkBoGccaZNRg5fWTp6dISA-pvD/view?usp=sharing")
-        }
+      <a
+        target="_blank"
+        href="https://drive.google.com/file/d/11O3VWVLkBoGccaZNRg5fWTp6dISA-pvD/view?usp=sharing"
       >
-        Download <img className="resume-icon" src={resume} alt="resume" />
-      </button>
+        <button className="resume-link d-block btn btn-primary">
+          Download <img className="resume-icon" src={resume} alt="resume" />
+        </button>
+      </a>
     </div>
   );
 };
